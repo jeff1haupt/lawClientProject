eae7d62c6c2c3f72d97d4b5f7707de7a785e127a
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-19 20:48:54 -0400

    Update CaseMatter.java
    Setting up variables for Class

--- src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
+++ src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
@@ -1,5 +1,14 @@
 package com.promineotech.lawClientProject.entity;
 
+import java.util.Set;
+
 public class CaseMatter {
+	
+	private Long id; 
+	private Set<Client> Client;
+	private Set<PracticeArea> PracticeArea;
+	private boolean isHourly;
+	private double flatFeeCharged;
+	private double retainerPaid;
 
 }
