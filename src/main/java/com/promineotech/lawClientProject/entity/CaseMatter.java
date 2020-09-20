226fe28dc3c12e305d62f3626029a4382cd0a7d2
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-19 20:55:34 -0400

    Update CaseMatter.java
    Added Getters and Setters

--- src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
+++ src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
@@ -5,13 +5,77 @@ import java.util.Set;
 
 public class CaseMatter {
 	
-
 	private Long id; 
 	private Date dateOpened;
 	private Set<Client> Client;
 	private Set<PracticeArea> PracticeArea;
+	private Set<TimeEntry> TimeEntry;
 	private boolean isHourly;
 	private double flatFeeCharged;
 	private double retainerPaid;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDateOpened() {
+		return dateOpened;
+	}
+
+	public void setDateOpened(Date dateOpened) {
+		this.dateOpened = dateOpened;
+	}
+
+	public Set<Client> getClient() {
+		return Client;
+	}
+
+	public void setClient(Set<Client> client) {
+		Client = client;
+	}
+
+	public Set<PracticeArea> getPracticeArea() {
+		return PracticeArea;
+	}
+
+	public void setPracticeArea(Set<PracticeArea> practiceArea) {
+		PracticeArea = practiceArea;
+	}
+
+	public Set<TimeEntry> getTimeEntry() {
+		return TimeEntry;
+	}
+
+	public void setTimeEntry(Set<TimeEntry> timeEntry) {
+		TimeEntry = timeEntry;
+	}
+
+	public boolean isHourly() {
+		return isHourly;
+	}
+
+	public void setHourly(boolean isHourly) {
+		this.isHourly = isHourly;
+	}
+
+	public double getFlatFeeCharged() {
+		return flatFeeCharged;
+	}
+
+	public void setFlatFeeCharged(double flatFeeCharged) {
+		this.flatFeeCharged = flatFeeCharged;
+	}
+
+	public double getRetainerPaid() {
+		return retainerPaid;
+	}
+
+	public void setRetainerPaid(double retainerPaid) {
+		this.retainerPaid = retainerPaid;
+	}
 
 }
