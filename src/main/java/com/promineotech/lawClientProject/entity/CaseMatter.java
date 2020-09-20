a4e850b46c4aecd0adfdf176fcfc56c30990c504
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-19 20:52:04 -0400

    Update CaseMatter.java

--- src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
+++ src/main/java/com/promineotech/lawClientProject/entity/CaseMatter.java
@@ -1,10 +1,13 @@
 package com.promineotech.lawClientProject.entity;
 
+import java.sql.Date;
 import java.util.Set;
 
 public class CaseMatter {
 	
+
 	private Long id; 
+	private Date dateOpened;
 	private Set<Client> Client;
 	private Set<PracticeArea> PracticeArea;
 	private boolean isHourly;
